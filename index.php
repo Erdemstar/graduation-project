64b61d41da837fd69710fb4b70d9bda33968ee89
Erdem STAR <erdem-yildiz@windoslive.com>
2018-10-02 11:02:39 +0300

    CSS/level5.php tasarım ve yazılımsal olarak çalışmaktadır

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
                         <li><h5><a href="CSS/level2.php">Seviye 2</a></h5></li>
                         <li><h5><a href="CSS/level3.php">Seviye 3</a></h5></li>
                         <li><h5><a href="CSS/level4.php">Seviye 4</a></h5></li>
-                        <li><h5><a href="#">Seviye 5</a></h5></li>
+                        <li><h5><a href="CSS/level5.php">Seviye 5</a></h5></li>
                         <li><h5><a href="#">Seviye 6</a></h5></li>
                         <li><h5><a href="#">Seviye 7</a></h5></li>
                         <li><h5><a href="#">Seviye 8</a></h5></li>
